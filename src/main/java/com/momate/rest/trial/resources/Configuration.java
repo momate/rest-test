d5dac584e70f0cf74a1e492b59404c2e75721a1e
momate <momate150@gmail.com>
2020-12-02 22:29:27 +0100

    Added dependencies and config class

---
+++ src/main/java/com/momate/rest/trial/resources/Configuration.java
@@ -0,0 +1,10 @@
+
+package com.momate.rest.trial.resources;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/api")
+class Configuration extends Application{
+    
+}
